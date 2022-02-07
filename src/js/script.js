e2c6f56b85cceaa782e8a04f5aefd56c25d1d6de
Yurii <yuriimazur1@gmail.com>
2022-02-07 14:31:42 +0200

    sending data to the server in XMLHttpRequest format, JSON

--- src/js/script.js
+++ src/js/script.js
@@ -256,13 +256,18 @@ window.addEventListener('DOMContentLoaded', () => {
 
             const request = new XMLHttpRequest();
             request.open('POST', 'server.php');
-            // request.setRequestHeader('Content-type', 'multipart/form-data');
+            request.setRequestHeader('Content-type', 'application/json');
 
-            //подготовка данных в формате form-data
-            //Объект form-data
             const formData = new FormData(form);
 
-            request.send(formData);
+            //подготовка данных в формате json
+            const object = {};
+            formData.forEach(function (value, key) {
+                object[key] = value;
+            });
+            const json = JSON.stringify(object);
+
+            request.send(json);
 
             request.addEventListener('load', () => {
                 if (request.status === 200) {
